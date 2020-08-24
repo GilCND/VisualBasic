613ca9045fdf09842b5520520a847a41fc87c60b
Felipe <69941457+GilCND@users.noreply.github.com>
2020-08-24 12:38:59 -0300

    Create HelloWorld.vb

---
+++ HelloWorld.vb
@@ -0,0 +1,13 @@
+'Program Name: Hello_World
+'Objkective: First contact with the language, Learn how to print something on the screen
+'Programmer: Felipe SG
+
+Module Module1
+
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.Write("Press Any Key to Continue...")
+        Console.ReadKey(True)
+    End Sub
+
+End Module
